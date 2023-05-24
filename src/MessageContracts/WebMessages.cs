0aa8bb2371911253fb91fc2f645edce24c479bda
Justin Mckenna <justin.k.mckenna@gmail.com>
2023-05-24 14:25:45 -0400

    apply for job event

--- src/MessageContracts/WebMessages.cs
+++ src/MessageContracts/WebMessages.cs
@@ -9,4 +9,11 @@
         public DateTimeOffset DateOfBirth { get; set; }
         public string EmailAddress { get; set; } = "";
     }
+
+    public class JobApplicationCreated
+    {
+        public static readonly string MessageId = "WebMessages.JobApplicationCreated";
+        public string ApplicantId { get; set; } = "";
+        public string JobOfferingId { get; set; } = "";
+    }
 }
--- src/web/JobApplicationMvc/JobApplicationMvc/Pages/JobDetails.cshtml.cs
+++ src/web/JobApplicationMvc/JobApplicationMvc/Pages/JobDetails.cshtml.cs
@@ -1,11 +1,14 @@
+using DotNetCore.CAP;
 using JobApplicationMvc.Areas.Identity.Data;
 using JobApplicationMvc.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using DomainEvents = MessageContracts;
 
 namespace JobApplicationMvc.Pages
 {
@@ -13,11 +16,13 @@ namespace JobApplicationMvc.Pages
     {
         private readonly JobApplicationsDataContext _context;
         private readonly UserManager<JobApplicationMvcUser> _userManager;
+        private readonly ICapPublisher _publisher;
 
-        public JobDetailsModel(JobApplicationsDataContext context, UserManager<JobApplicationMvcUser> userManager)
+        public JobDetailsModel(JobApplicationsDataContext context, UserManager<JobApplicationMvcUser> userManager, ICapPublisher publisher)
         {
             _context = context;
             _userManager = userManager;
+            _publisher = publisher;
         }
 
         [BindProperty]
@@ -25,6 +30,7 @@ namespace JobApplicationMvc.Pages
 
         public JobOpeningEntity? Job { get; set; }
         public JobApplicationMvcUser? Applicant { get; set; }
+        [BindProperty]
         public string UserId { get; set; } = string.Empty;
         public string? Message { get; set; }
         public async Task OnGetAsync(int id)
@@ -50,10 +56,38 @@ namespace JobApplicationMvc.Pages
             }
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int id)
         {
-            
-            return RedirectToPage("/myapplications");
+            var userId = User.Claims.SingleOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value!;
+            UserId = userId;
+            var email = User?.Identity?.Name;
+            Applicant = await _userManager.FindByIdAsync(userId);
+            var opening = await _context.JobOpenings
+            .Where(o => o.Id == id)
+            .SingleOrDefaultAsync();
+            if (opening is null)
+            {
+                Message = "Uh Oh! That Opening Doesn't Exist or has been filled!";
+            }
+            else
+            {
+                Job = opening;
+                ApplicationModel.JobId = Job.OpeningId.ToString();
+                ApplicationModel.UserId = Applicant.Id;
+
+
+
+            }
+            var eventToPublish = new DomainEvents.JobApplicationCreated
+            {
+                ApplicantId = UserId,
+                JobOfferingId = opening.OpeningId.ToString()
+
+
+
+            };
+            await _publisher.PublishAsync(DomainEvents.JobApplicationCreated.MessageId, eventToPublish);
+            return RedirectToPage("Index");
         }
 
 
